5925db983dd6e9d27812923266f6f14be69c4f6d
dae-ne <rafalcz29@gmail.com>
2023-12-03 13:44:26 +0100

    Add project files

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# dotnet-mailkit-example
+# .NET MailKit Example
+
+This example shows how to use the [MailKit](https://github.com/jstedfast/MailKit)
+library to send emails from a .NET application.
+
+## Prerequisites
+
+- .NET 8.0 or later
+- Gmail account
+- Gmail app password (see [How to generate an app password](https://support.google.com/accounts/answer/185833))
+
+## Running the example
+
+1. Clone the repository
+2. Edit the `Program.cs` file and replace the following values:
+   - `senderEmail`: the email address of the Gmail account you want to send emails from
+   - `senderName`: the name of the sender
+   - `senderPassword`: the app password generated for the Gmail account
+   - `recipientEmail`: the email address of the recipient
+   - `recipientName`: the name of the recipient
+3. Run the example:
+    ```bash
+    dotnet run
+    ```
+4. Check the inbox of the recipient email address
+
+## License
+
+This example is licensed under the [MIT License](LICENSE).
---
+++ src/Program.cs
@@ -0,0 +1,53 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+const string host = "smtp.gmail.com";
+const int port = 587;
+
+const string senderEmail = "";
+const string senderName = "";
+const string password = "";
+
+const string recipientEmail = "";
+const string recipientName = "";
+
+const string subject = "How you doin'?";
+const string body = $"""
+    Hey,
+    I am just writing to let you know that I am still alive.
+    -- {senderEmail}
+    """;
+
+await SendEmailAsync(
+    senderEmail,
+    senderName,
+    password,
+    recipientEmail,
+    recipientName,
+    subject,
+    body);
+
+return;
+
+static async Task SendEmailAsync(
+    string senderEmail,
+    string senderName,
+    string password,
+    string recipientEmail,
+    string recipientName,
+    string subject,
+    string body)
+{
+    using var message = new MimeMessage();
+    message.From.Add(new MailboxAddress(senderName, senderEmail));
+    message.To.Add(new MailboxAddress(recipientName, recipientEmail));
+    message.Subject = subject;
+    message.Body = new TextPart("plain") { Text = body };
+
+    using var client = new SmtpClient();
+    await client.ConnectAsync(host, port, SecureSocketOptions.StartTls);
+    await client.AuthenticateAsync(senderEmail, password);
+    await client.SendAsync(message);
+    await client.DisconnectAsync(true);
+}
